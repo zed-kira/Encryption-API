444d2bdafb3fd5a0ec5813297afd1a0b56e862ff
zed-kira <admin@zedkira.com>
2023-02-04 09:35:17 +0100

    initial UI

---
+++ frontend/src/scenes/global/Sidebar/index.jsx
@@ -0,0 +1,17 @@
+// React
+import React, { useState } from 'react';
+
+// Ant Design
+import { Layout, Menu, theme } from 'antd';
+
+export default Sidebar = () => {
+
+    return (
+        <>
+        
+            <div>Hello from Sidebar</div>
+
+        </>
+    )
+
+}
